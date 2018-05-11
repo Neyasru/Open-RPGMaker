c718a727a177486f45a21f34e3b63464689835d3
ferriperello <ferriperello@gmail.com>
2018-05-11 20:20:07 +0200

    Image reference + 2 test de tile

---
+++ windows/MVS/ScriptRPG/assets/scripts/grass.lua
@@ -0,0 +1,16 @@
+
+player = {
+    GraphicsComponent = {
+        imageID = "img_tileset";
+		row = 11;
+		column = 1;
+    },
+	TransformComponent = {
+		w = 64;
+		h = 64;
+		scale = 1;
+	},
+	Collisionable{
+		collision = false
+	},
+}
---
+++ windows/MVS/ScriptRPG/assets/scripts/sea.lua
@@ -0,0 +1,16 @@
+
+sea = {
+    GraphicsComponent = {
+        imageID = "img_tileset";
+		row = 10;
+		column = 3;
+    },
+	TransformComponent = {
+		w = 64;
+		h = 64;
+		scale = 1;
+	},
+	Collisionable{
+		collision = true
+	},
+}
